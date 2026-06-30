f32b424cf309d5f33a4aec97fdf8e003e9ce9b47
LaxmiMishra005 <mishrasweety005@gmail.com>
2026-06-30 23:36:39 +0530

    added login and register page

--- 2.js
+++ 2.js
@@ -10,15 +10,29 @@ if (addCartBtn) {
 // Login Form
 const loginForm = document.getElementById("loginForm");
 
-if (loginForm) {
+if(loginForm){
 
-    loginForm.addEventListener("submit", function (e) {
+    loginForm.addEventListener("submit", function(e){
 
         e.preventDefault();
 
-        alert("Login Successful!");
+        const email=document.getElementById("loginEmail").value;
+        const password=document.getElementById("loginPassword").value;
 
-        window.location.href = "index.html";
+        const savedEmail=localStorage.getItem("userEmail");
+        const savedPassword=localStorage.getItem("userPassword");
+
+        if(email===savedEmail && password===savedPassword){
+
+            alert("Login Successful!");
+
+            window.location.href="index.html";
+
+        }else{
+
+            alert("Invalid Email or Password!");
+
+        }
 
     });
 
@@ -29,13 +43,32 @@ const registerForm = document.getElementById("registerForm");
 
 if (registerForm) {
 
-    registerForm.addEventListener("submit", function (e) {
+    registerForm.addEventListener("submit", function(e) {
 
         e.preventDefault();
 
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("regEmail").value;
+        const password = document.getElementById("regPassword").value;
+        const confirmPassword = document.getElementById("confirmPassword").value;
+
+        if(name==="" || email==="" || password==="" || confirmPassword===""){
+            alert("Please fill all fields.");
+            return;
+        }
+
+        if(password !== confirmPassword){
+            alert("Passwords do not match.");
+            return;
+        }
+
+        localStorage.setItem("userName", name);
+        localStorage.setItem("userEmail", email);
+        localStorage.setItem("userPassword", password);
+
         alert("Registration Successful!");
 
-        window.location.href = "Login.html";
+        window.location.href="Login.html";
 
     });
 
